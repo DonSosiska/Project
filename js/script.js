0d5a60fc923b51d86bc49aac643f1b8fa9e3dafd
DonSosiska <don.sosiska339@gmail.com>
2020-06-01 00:53:55 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//alert("fuck u");
+
+// let rez = confirm("R u gay?");
+
+// const answer = +prompt("Vam est` 18?", "No");
+// console.log(answer + 8);
+
+const answer = [];
+answer[0] = prompt("Как Вас зовут?", "");
+answer[1] = prompt("Фамилия?", "");
+answer[2] = prompt("Сколько Вам лет?", "");
+
+document.write(answer);
